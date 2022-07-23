58a869b3554cee317f534593b024028c6d58326b
Barath <98839273+Barathlzzy@users.noreply.github.com>
2022-07-23 17:05:59 +0530

    1st upload

---
+++ main.js
@@ -0,0 +1,6 @@
+alert('test'):
+// window.addEventListener("scroll", function(){
+//   const header = document.querySelector("header");
+//   header.classList.toggle("sticky", window.scrollY>0);
+//   }
+// })
